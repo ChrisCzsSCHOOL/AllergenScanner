3eb00040d03617250334b355d960529035f86331
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2025-05-17 23:22:37 +0200

    Het werkt!

--- frontend/allergen-scanner/src/components/Allergens.jsx
+++ frontend/allergen-scanner/src/components/Allergens.jsx
@@ -1,8 +1,12 @@
 function Allergens({ allergens }) {
-  return (
+  
+  const allergenString = allergens.toString();
+  
+  
+    return (
     <div className="mt-20 flex flex-col items-center justify-center">
       <h2 className="text-xl font-semibold mb-4">Allergens</h2>
-        <p></p>
+        <p>{allergenString}</p>
     </div>
   );
 }
