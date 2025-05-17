a51d3931696f74005e134861ace34e9148ac4d1f
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2025-05-17 21:46:40 +0200

    tailwind install

---
+++ frontend/allergen-scanner/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <p className="text-xl font-semibold underline">Hello world!</p>
+    </>
+  );
+}
+
+export default App;
