47c0e6b60ab048c21ea0b57c9083dd5e18e9ae41
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2025-05-17 21:52:52 +0200

    Barcode import

--- frontend/allergen-scanner/src/App.jsx
+++ frontend/allergen-scanner/src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BarcodeScanner from "react-qr-barcode-scanner";
 import "./App.css";
 
+
 function App() {
+  const [data, setData] = useState("Not Found");
+  const [barcode, setBarcode] = useState(null);
+
+  useEffect(() => {
+    if (data !== "Not Found") {
+      setBarcode(data);
+    }
+  }, [data]);
 
   return (
     <>
-      <p className="text-xl font-semibold underline">Hello world!</p>
+      {/* div voor de hele app */}
+      <div className="flex flex-col items-center justify-center h-screen bg-blue-100">
+        {/* div voor header */}
+        <div>
+          <h1 className="text-2xl font-semibold underline">
+            Allergen scanner!
+          </h1>
+        </div>
+
+        {/* div voor scanner */}
+        <div>
+          <BarcodeScanner
+            width={500}
+            height={500}
+            onUpdate={(err, result) => {
+              if (result) setData(result.text);
+              else setData("Not Found");
+            }}
+          />
+          <p>{barcode}</p>
+        </div>
+      </div>
     </>
   );
 }
