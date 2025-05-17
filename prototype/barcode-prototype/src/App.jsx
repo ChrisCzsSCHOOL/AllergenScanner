13bde477aeabc20a4af85eacd71fef7405ad20db
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2025-05-17 21:30:34 +0200

    werkt met barcode

--- prototype/barcode-prototype/src/App.jsx
+++ prototype/barcode-prototype/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import BarcodeScanner from "react-qr-barcode-scanner";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState("Not Found");
+  const [barcode, setBarcode] = useState(null);
+
+  useEffect(() => {
+    if (data !== "Not Found") {
+      setBarcode(data);
+    }
+  }, [data]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BarcodeScanner
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{barcode}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
